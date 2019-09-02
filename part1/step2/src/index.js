cb4743aa743c2f7f171f3896470da2c42e8d00f3
Ilham Alamsyah <ilham@alamat.com>
2019-09-02 10:58:25 +0700

    third commit

--- part1/step2/src/index.js
+++ part1/step2/src/index.js
@@ -4,51 +4,48 @@ import ReactDOM from 'react-dom'
 const Header = (props) => {
     return (
         <div>
-       <h1>
-       {props.course}
-       </h1>
+            <h1>
+            {props.course}
+            </h1>
         </div>
     )
 }
 
-const Content = (props) => {
+const Content = (data) => {
     return(
         <div>
-            <p>{props.part1.part2.part3}
-             </p>
-            
+            <p>{data.part1}</p>
+            <p>{data.part2}</p>
+            <p>{data.part3}</p>
+            <img src = 'https://mildaintrainings.com/wp-content/uploads/2017/11/react-logo.png'></img>
         </div>
     )
 }
 
-const Total = (props) => {
+const Total = (value) => {
     return(
         <div>
-            <p>
-                {props.exercise1.exercise2.exercise3}
-            </p>
+            {value.exercise1}
+            {value.exercise2}
+            {value.exercise3}
         </div>
     )
 }
 
 const App = () => {
     const course = 'Half Stack application development'
-    const part1  = 'Fundamental of React'
-    const part2  = 'Using props to pass data'
-    const part3  = 'State of component'
-    const exercise1 = 10
-    const exercise2 = 7
-    const exercise3 = 14
 
     return(
     <div>
         <Header course = {course} />
-        <Content part1 = {part1} />
-        <Content part2 = {part2} />
-        <Content part3 = {part3} />
-        <Total exercise1 = {exercise1} />
-        <Total exercise2 = {exercise2} />
-        <Total exercise3 = {exercise3} />
+       <Content part1 = 'Fundamental of React'
+                part2 = 'Using props to pass data'
+                part3 = ' State of a component'
+       />
+       <Total exercise1 = '10'
+              exercise2 = '7'
+              exercise3 = '14'
+       />
     </div>
     )
 }
